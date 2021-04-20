9184f1f1db923545ed53f017af06506baa0cb747
97yunus97 <yunusballi2@gmail.com>
2021-04-20 21:21:58 +0300

    Initial commit

---
+++ src/test/java/org/example/TestUygulama.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestUygulama {
+
+    public static void main(String[] args){
+
+        System.setProperty()
+
+
+    }
+
+    }
+
+
